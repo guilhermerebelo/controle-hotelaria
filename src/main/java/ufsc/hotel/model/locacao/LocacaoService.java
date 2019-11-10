235c85a931eb39199d07525d1d7f7bd46718ab92
Guilherme Rebelo <guilhermerebelo@live.com>
2019-11-10 16:07:35 -0300

    criando service para exibir as informaões gerais

--- src/main/java/ufsc/hotel/model/locacao/LocacaoService.java
+++ src/main/java/ufsc/hotel/model/locacao/LocacaoService.java
@@ -2,47 +2,61 @@ package ufsc.hotel.model.locacao;
 
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
 @Service
 public class LocacaoService {
 
-    /*
-    Elaboração e apresentação de um conjunto de consultas que sumarizem as informações
-    partir de cada uma das consultas (elaborar no mínimo 3 (três) consultas) gráficos devem
-    do banco de dados envolvendo algumas (mais de duas) tabelas e funções de agregação.
+    private EntityManager em;
+
+    public LocacaoService(EntityManager em) {
+        this.em = em;
+    }
+
+    //donat
+    public void taxaOcupacaoDosQuartos() {
+        Query query = em.createQuery("select * from a");
+//        List resultList = query.getResultList();
+//        Object singleResult = query.getSingleResult();
+
+        //retornar um mapa com descrição do quarto e total..
+    }
+
+    //donat
+    public void produtosMaisConsumidos() {
+        Query query = em.createQuery("select * from a");
+//        List resultList = query.getResultList();
+//        Object singleResult = query.getSingleResult();
+
+        //retornar um mapa com o total de consumo no mes e a descrição do produto
+    }
+
+    //donat
+    public void locacaoPorFuncionario() {
+        Query query = em.createQuery("select * from a");
+//        List resultList = query.getResultList();
+//        Object singleResult = query.getSingleResult();
+
+        //retornar um mapa com funcionario e quantidade..
+    }
 
 
-    A ser produzidos. Deve-se ainda prover uma descrição do objetivo de cada consulta, assim
-    como, uma pequena amostra do resultado, ou seja, um conjunto de linhas recuperadas a
-    partir da consulta (1,0 pontos);
-    */
+    //grafico em linha
+    public void porcengagemOcupacaoHotel() {
+        Query query = em.createQuery("select * from a");
+//        List resultList = query.getResultList();
+//        Object singleResult = query.getSingleResult();
 
-    /*
-     * Elaborar 3 graficos com pelo menos 2 ou 3 tabelas, e com funão de agregação
-     *
-     * Media valor locacao mensal / criar um grafico com os ultimos meses
-     * ticket medio de uma locacao / criar um grafico com os ultimos meses
-     * porcentagem de ocupação do hotel / criar um grafico com os ultimos meses
-     *
-     * quarto mais locado
-     * funcionarios que fizeram mais locacao
-     *
-     *
-     * */
+        //porcentagem de ocupação dos ultimos 5 meses
+    }
 
-//    private LocacaoRepository repository;
-//    private EntityManager em;
+    //grafico em linha
+    public void ticketMedioPorLocacao() {
+        Query query = em.createQuery("select * from a");
+//        List resultList = query.getResultList();
+//        Object singleResult = query.getSingleResult();
 
-//    public LocacaoService(LocacaoRepository repository, EntityManager em) {
-//        this.repository = repository;
-//        this.em = em;
-//    }
-//
-//    public BigDecimal mediaValorLocacaoMensal() {
-//        Query query = em.createQuery("select * formt a");
-//        query.getResultList();
-//    }
-//
-//    public BigDecimal mediaValorLocacaoMensal() {
-//        Query query = em.creat
-//    }
+        //ticket medio de locação dos ultimos 5 meses
+    }
 }
